06743efa7cd8818a6d5c6f5472505135b0e6baf4
Boming Kong <michaelkongboming@gmail.com>
2025-03-04 12:22:41 -0800

    Fixed yosys gls target

--- synth/yosys_generic/top_module_sim_gls.sv
+++ synth/yosys_generic/top_module_sim_gls.sv
@@ -4,9 +4,6 @@ module top_module_sim_gls#(
     ) (
     input   logic clk_i,
     input   logic rst_ni,
-    input   logic uart_req_val_i,
-    input   logic [UART_DATA_WIDTH-1: 0] uart_data_i,
-    output  logic uart_req_rdy_o,
 
     input   logic uart_rsp_rdy_i,
     output  logic [UART_DATA_WIDTH-1: 0] uart_rsp_data_o,
@@ -18,9 +15,6 @@ top_module #(
 ) top_module (
 		.clk_i(clk_i),
 		.rst_ni(rst_ni),
-		.uart_req_val_i(uart_req_val_i),
-		.uart_data_i(uart_data_i),
-		.uart_req_rdy_o(uart_req_rdy_o),
 
 		.uart_rsp_rdy_i(uart_rsp_rdy_i),
 		.uart_rsp_data_o(uart_rsp_data_o),
