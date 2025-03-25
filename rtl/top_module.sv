e0c0126bc54bd08a74ec0cdc6edfeb6bdb950591
Boming Kong <michaelkongboming@gmail.com>
2025-03-25 17:18:09 -0400

    Start working on 32bits uart output

--- rtl/top_module.sv
+++ rtl/top_module.sv
@@ -1,19 +1,19 @@
 module top_module#(
-    parameter UART_DATA_WIDTH = 8
+    parameter DATAW = 32
     ) (
     input   logic clk_i,
     input   logic rst_ni,
 
     input   logic uart_rsp_rdy_i,
-    output  logic [UART_DATA_WIDTH-1: 0] uart_rsp_data_o,
+    output  logic [DATAW-1: 0] uart_rsp_data_o,
     output  logic uart_rsp_val_o
 );
 
 
-    logic [UART_DATA_WIDTH-1: 0]uart_buf_q;
-    logic [UART_DATA_WIDTH-1: 0]uart_buf_d;
+    logic [DATAW-1: 0]uart_buf_q;
+    logic [DATAW-1: 0]uart_buf_d;
     
-    logic [UART_DATA_WIDTH-1: 0]send_out;
+    logic [DATAW-1: 0]send_out;
 
     assign uart_rsp_data_o = send_out;
 
@@ -22,7 +22,7 @@ module top_module#(
         send_out = 0;
         if (uart_rsp_rdy_i) begin
             send_out = uart_buf_q;
-            uart_buf_d = uart_buf_q > 8'h59 ? 8'h31 : uart_buf_q + 1;
+            uart_buf_d = uart_buf_q > 32'habcd ? 32'hab40 : uart_buf_q + 1;
             uart_rsp_val_o = 1;
         end else begin
             uart_buf_d = uart_buf_q;
@@ -31,7 +31,7 @@ module top_module#(
         
     always @(posedge clk_i) begin
         if (~rst_ni) begin
-            uart_buf_q <= 8'h31;
+            uart_buf_q <= 32'hab40;
         end else begin
             uart_buf_q <= uart_buf_d;
         end
