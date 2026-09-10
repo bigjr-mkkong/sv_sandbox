e26215f032d3bebb75058c0695318581fde19a88
Boming Kong <michaelkongboming@gmail.com>
2026-09-10 02:37:15 -0400

    Added new openroad script

---
+++ openroad/launch_openroad.sh
@@ -0,0 +1,8 @@
+OR_IMAGE=openroad/orfs:latest \
+../../OpenROAD-flow-scripts/flow/util/docker_shell \
+make -f /OpenROAD-flow-scripts/flow/Makefile \
+DESIGN_CONFIG=/work/config.mk \
+WORK_HOME=/work/output \
+open_place
+
+# Replace above with gui_floorplan for gui inspection
